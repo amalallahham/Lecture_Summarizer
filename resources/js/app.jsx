b2efce3f885396b297fb05d0c285826904841148
ZhaoqiuD <dingzq0807@gmail.com>
2025-04-10 01:07:46 -0700

    updated lecture summmary

---
+++ resources/js/app.jsx
@@ -0,0 +1,7 @@
+// app.js (if using React)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../../src/App'
+import '../css/app.css'
+import '../css/dashboard.css'; 
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
